2e52a764c895930f357e8ea69ae75932ecbc7a3b
N3r0m5ns3r <ziadh6@hotmail.com>
2021-01-12 17:22:04 +0100

    Enum

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,5 +1,17 @@
 defmodule Cards do
-  def create_check do
-    ["Ace", "Two", "Three"]
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suits <- suits do 
+      suit
+  end
+  
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
